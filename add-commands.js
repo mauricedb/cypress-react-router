11880b1696189bc6af0b96779433f29f210ce6fc
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-12-31 15:35:50 +0100

    import 'cypress-react-router/add-commands'

---
+++ add-commands.js
@@ -0,0 +1,4 @@
+// this file is here so it's easy to register the commands
+// `import 'cypress-react-router/add-commands';`
+const { addReactRouterHistoryCommand } = require('./dist');
+addReactRouterHistoryCommand();
